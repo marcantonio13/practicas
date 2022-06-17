b762cebc99b3572745b51de6787452c0eb1475eb
Marco Antonio <antonio.avellaneda13@gmail.com>
2022-06-17 15:16:02 -0500

    actualice

--- saludo/script.js
+++ saludo/script.js
@@ -1,1 +1,17 @@
 console.log("Se nota qu todavia me extrañas");
+
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo() {
+  //console.log("Me parece una falta de respecto");
+  let frase = document.getElementsByTagName("h2")[1].innerHTML;
+
+  if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+    // frase = "Impongo moda para las niñas!";
+   document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+  } else {
+    //frase = "Ganando como siempre";
+    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+  }
+  console.log("frase :" + frase);
+}
